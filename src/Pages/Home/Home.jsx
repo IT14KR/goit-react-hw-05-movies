7a9368c7c68a5dfd82ab5bf54442206da46a82d3
IT14KR <131267129+IT14KR@users.noreply.github.com>
2023-09-04 17:44:06 +0200

    committ

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { MovieLink, MovieList, Title } from './Home.styled';
+
+import { Loader } from 'components/Loader/Loader';
+import { getTrending } from 'Api/apiService';
+
+const Home = () => {
+  const [trendMovies, setTrendMovies] = useState([]);
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTrending().then(data => {
+      setTrendMovies(data.resulsts);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    <main>
+      <Title>Top Movies for today</Title>
+      <MovieList>
+        {trendMovies.map(movie => (
+          <li key={movie.id}>
+            <MovieLink to={`/movies/${movie.id}`} state={{ from: location }}>
+              {movie.original_title || movie.name}
+            </MovieLink>
+          </li>
+        ))}
+        {isLoading && <Loader />}
+      </MovieList>
+    </main>
+  );
+};
+
+export default Home;
